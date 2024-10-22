afba1a678b3829fcb250aa16aff7d107800f02ac
Lokendra Jirel <lokendra.b.jirel@gmail.com>
2024-10-22 08:38:09 +0545

    first commit

---
+++ src/test/java/edu/divyagyan/main/DoctorAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.divyagyan.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
